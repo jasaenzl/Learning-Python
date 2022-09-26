b954cf99abbc81a8d72f04c41618a5d728d50d21
Javier Sáenz <jasaenzl@unal.edu.co>
2022-09-26 11:49:07 -0500

    Init version

--- HelloWorld/helloworld.py
+++ HelloWorld/helloworld.py
@@ -1,6 +1,6 @@
 # This is my first Python Program
 # this is a commentary line
 # you can comment the code using the # character
-# this programs writes the string hello world in the terminal
+# this program writes the string "Hola Mundo!"" in the terminal
 
 print("Hola Mundo!")
