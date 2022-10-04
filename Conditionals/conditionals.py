f4cc7998828dbf88a0a09261c6eeaa3422d21ed2
Javier Sáenz <jasaenzl@unal.edu.co>
2022-10-04 14:07:13 -0500

    fix indentation

--- Conditionals/conditionals.py
+++ Conditionals/conditionals.py
@@ -18,4 +18,4 @@ elif bmi<25 :
 elif bmi<30 :
     print (f"Su indice de masa corporal es: {round(bmi,1)}, lo que indica que tiene sobrepeso")
 else:
-     print (f"Su indice de masa corporal es: {round(bmi,1)}, lo que indica que tiene obesidad")
+    print (f"Su indice de masa corporal es: {round(bmi,1)}, lo que indica que tiene obesidad")
